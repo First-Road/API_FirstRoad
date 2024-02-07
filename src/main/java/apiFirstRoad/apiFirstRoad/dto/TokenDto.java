d4e760c22bc7e51f7007fef4b1a668581e9ba83e
EvertonAraujo88 <evertonaraujosenai@gmail.com>
2024-02-07 19:19:32 -0300

    deletando

--- src/main/java/apiFirstRoad/apiFirstRoad/dto/TokenDto.java
+++
@@ -1,4 +0,0 @@
-package apiFirstRoad.apiFirstRoad.dto;
-
-
-public record TokenDto(String token) {}
--- src/main/java/apiFirstRoad/apiFirstRoad/dto/UnidadeOrganizacionalDto.java
+++
@@ -1,9 +0,0 @@
-package apiFirstRoad.apiFirstRoad.dto;
-
-import java.util.UUID;
-
-public record UnidadeOrganizacionalDto(
-        String nome_departamento,
-        UUID id_usuario,
-        UUID id_unidade
-) {}
